3b8929a53d0efacaff208129fade3a8cbdaa04f9
Alzater <alzater@mail.ru>
2016-01-09 21:45:37 +0300

    add main menu scene

--- src/scenes/MainMenuScene.cpp
+++ src/scenes/MainMenuScene.cpp
@@ -4,4 +4,10 @@
 MainMenuScene::MainMenuScene()
 {
     _menu->addItem(std::string("EATING CIRCLES"));
+    _menu->addItem(std::string("NEW GAME"), [](Event *e){
+            Controller::getController()->changeScene(Scenes::GAME_SCENE);
+        });
+    _menu->addItem(std::string("EXIT"), [](Event *e){
+            core::requestQuit();
+        });
 }
